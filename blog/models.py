112c717c631189796f45f3fe1bb11553e552bb41
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-19 22:24:53 +0200

    posts url

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from telnetlib import STATUS
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -10,6 +11,14 @@ class PublishedManager(models.Manager):
                         .filter(status='published')
 
 class Post(models.Model):
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.month,
+                              self.publish.day, self.slug])
+
+
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published')
