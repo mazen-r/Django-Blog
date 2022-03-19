6ac8f72042231bbc78da0a75e8404a268a4c0867
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-19 22:02:56 +0200

    post view

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,19 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                    self).get_queryset()\
+                        .filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published')
     )
+    objects = models.Manager()
+    published = PublishedManager()
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
                             unique_for_date='publish')
@@ -28,3 +35,5 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+
